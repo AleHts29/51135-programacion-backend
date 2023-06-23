0181c7adf8700d32dece38719f597e9461fc7fbd
Alejandro Huertas <ahuertas@fi.uba.ar>
2023-06-22 21:06:43 -0300

    Clase34

--- Clase_34/src/app.js
+++ Clase_34/src/app.js
@@ -6,13 +6,17 @@ import MongoSingleton from './config/mongodb-singleton.js';
 import performanceRouter from './routers/performance-test.router.js';
 import sessionRouter from './routers/sessions.router.js'
 import userRouter from './routers/users.router.js';
+
+// import { addLogger } from './config/logger_BASE.js';
 import { addLogger } from './config/logger.js';
 
 const app = express();
 
 //JSON settings:
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
+
+// Middleware
 app.use(addLogger);
 
 
@@ -21,7 +25,9 @@ app.use("/api/performance", performanceRouter);
 app.use("/api/session", sessionRouter);
 app.use("/api/user", userRouter);
 
-app.get("/logger", (req, res)=>{
+// Endpoint de prueba
+app.get("/logger", (req, res) => {
+    // req.logger.warn("Prueba de log level warning!");
     req.logger.warning("Prueba de log level warning!");
     res.send("Prueba de logger!");
 });
